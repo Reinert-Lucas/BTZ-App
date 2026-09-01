0e84eeac7876df835a5d01620e5a28fbde5b6429
Reinert-Lucas <lucasreinert2006@gmail.com>
2026-09-01 17:22:24 -0300

    Agregado: - Botones de Crear y Home en vista usuarios (Falta replicar en todas)

--- server/resources/views/admin/usuarios/index.blade.php
+++ server/resources/views/admin/usuarios/index.blade.php
@@ -4,10 +4,14 @@
     <x-text title="Gestion de Usuarios"></x-text>
 @endsection
 @section('content')
-    <section>
-        <a href={{ route('admin.dashboard') }}>Inicio</a>
-        <a class="btn btn-primary" href={{ route('admin.usuarios.create') }}>+</a>
-        <x-adv :filtros="$filtros" ruta="usuarios" model="usuario"></x-adv>
+    <section class="btns-section">
+        <a class="home-btn" href={{ route('admin.dashboard') }}>
+            <img src="{{ asset('imgs/home.png') }}" alt="Volver al Inicio">
+        </a>
+        <a class="create-btn" href={{ route('admin.usuarios.create') }}>
+            <img src="{{ asset('imgs/add.png') }}" alt="Crear Nuevo">
+        </a>
     </section>
+    <x-adv :filtros="$filtros" ruta="usuarios" model="usuario"></x-adv>
     <x-table :columns="$columns" :rows="$usuarios" parameter="usuario" resource="usuarios" />
 @endsection
